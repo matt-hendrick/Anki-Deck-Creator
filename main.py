e407e872b6c4ada862110b750d4b4158266317d8
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2022-04-12 20:42:19 -0400

    Scraper now downloads mp3 and adds to deck

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
-from dateutil import parser
-import uuid
-import datetime
+import time
+import requests
 import genanki
+import os.path
 
 from get_soup import get_soup
 from remove_prefix import remove_prefix
@@ -12,7 +12,7 @@ def main():
     base_url = "https://www.goethe-verlag.com/book2/"
     new_deck = genanki.Deck(
         2059400110,
-        "Animals")
+        "Hindi Animals Test Deck")
 
     soup = get_soup(
         "https://www.goethe-verlag.com/book2/_VOCAB/EM/EMHI/02.HTM")
@@ -21,17 +21,39 @@ def main():
         english = phrase.find("span", class_="Stil36").get_text()
         hindi = phrase.find("span", class_="Stil46").get_text()
         transliteration = phrase.find("span", class_="Stil39").get_text()
+
+        # clean up image url
         img_url = base_url + \
             remove_prefix(phrase.find("img").get("src"), "../../../")
-        audio_url = "[sound:" + base_url + remove_prefix(phrase.find(
-            "source").get("src"), "../../../") + "]"
-        card_id = str(uuid.uuid4())
-        # card_list.append({"english": english, "hindi": hindi,
-        #                 "transliteration": transliteration, "img_url": img_url, "audio_url": audio_url, "card_id": card_id})
-       #   print(card_list[-1])
+
+        # TODO: Download jpg instead of using remote image
+        # image_file_name = img_url.split("/")[-1]
+        # doc = requests.get(img_url)
+        # with open(image_file_name, 'wb') as f:
+        #     f.write(doc.content)
+        # anki_formatted_image = f'<img src="{image_file_name}">'
+
+        # clean up audio url and download mp3
+        audio_cleaned_path = remove_prefix(phrase.find(
+            "source").get("src"), "../../../")
+        audio_file_name = audio_cleaned_path.split("/")[-1]
+        if not os.path.isfile(audio_file_name):
+            audio_url = base_url + audio_cleaned_path
+            doc = requests.get(audio_url)
+
+            with open(audio_file_name, 'wb') as f:
+                f.write(doc.content)
+
+        anki_formatted_audio = "[sound:" + audio_file_name + "]"
+
+        # try not to clobber the server by mass downloading a bunch of files
+        time.sleep(1)
+
+        # create card and add to deck
         new_note = genanki.Note(
-            anki_card_model, [english, hindi, transliteration, audio_url, img_url])
+            anki_card_model, [english, hindi, transliteration, anki_formatted_audio, img_url])
         new_deck.add_note(new_note)
+
         print(new_note)
         print("\n")
 
