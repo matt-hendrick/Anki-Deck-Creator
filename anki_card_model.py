460c769a63849e6f2111ee2026e3ff92a9c1fc95
Matt Hendrick <75456865+matt-hendrick@users.noreply.github.com>
2022-04-14 20:47:50 -0400

    WIP - Function (but rough atm) scraper/Anki deck creator for Goethe Verlag's Hindi phrases

--- anki_card_model.py
+++ anki_card_model.py
@@ -27,12 +27,12 @@ anki_card_model = genanki.Model(
         {
             'name': 'Card 1',
             'qfmt': '{{English}}',
-            'afmt': '{{FrontSide}}\n\n<hr id=answer>\n\n{{Hindi}}\n\n{{Transliteration}}\n\n<img src={{Image}}><br>{{Audio}}',
+            'afmt': '{{FrontSide}}\n\n<hr id=answer>\n\n{{Hindi}}<br>{{Transliteration}}<br>{{Image}}<br>{{Audio}}',
         },
         {
             'name': 'Card 2',
-            'qfmt': '{{Hindi}}\n\n{{Transliteration}}<br>{{Audio}}',
-            'afmt': '{{FrontSide}}\n\n<hr id=answer>\n\n{{English}}\n\n<img src={{Image}}>',
+            'qfmt': '{{Hindi}}<br>{{Transliteration}}<br>{{Audio}}',
+            'afmt': '{{FrontSide}}\n\n<hr id=answer>\n\n{{English}}<br>{{Image}}',
         },
     ],
     css='.card {\n font-family: arial;\n font-size: 20px;\n text-align: center;\n color: black;\n background-color: white;\n}\n',
--- main.py
+++ main.py
@@ -10,54 +10,81 @@ from anki_card_model import anki_card_model
 
 def main():
     base_url = "https://www.goethe-verlag.com/book2/"
+    # TODO: Allow passing in variables for both deck name and language
     new_deck = genanki.Deck(
         2059400110,
-        "Hindi Animals Test Deck")
+        "Hindi Deck")
 
-    soup = get_soup(
-        "https://www.goethe-verlag.com/book2/_VOCAB/EM/EMHI/02.HTM")
+    list_of_media_files = []
 
-    for phrase in soup.find_all("div", class_="col-sm-3"):
-        english = phrase.find("span", class_="Stil36").get_text()
-        hindi = phrase.find("span", class_="Stil46").get_text()
-        transliteration = phrase.find("span", class_="Stil39").get_text()
+    # TODO: Dynamically get all pages for a language instead of getting fixed range
+    # iterates through each category of phrases
+    for page_number in range(1, 43):
+        # TODO: rework the need for this
+        if page_number < 10:
+            page_number = f"0{page_number}"
 
-        # clean up image url
-        img_url = base_url + \
-            remove_prefix(phrase.find("img").get("src"), "../../../")
+        page_url = f"https://www.goethe-verlag.com/book2/_VOCAB/EM/EMHI/{page_number}.HTM"
 
-        # TODO: Download jpg instead of using remote image
-        # image_file_name = img_url.split("/")[-1]
-        # doc = requests.get(img_url)
-        # with open(image_file_name, 'wb') as f:
-        #     f.write(doc.content)
-        # anki_formatted_image = f'<img src="{image_file_name}">'
+        # check page and throw error if page returns anything but 200
+        page_status = requests.get(page_url).status_code
+        if page_status != 200:
+            raise Exception(
+                f"HTTP error for {page_url}. Page status = {page_status}")
 
-        # clean up audio url and download mp3
-        audio_cleaned_path = remove_prefix(phrase.find(
-            "source").get("src"), "../../../")
-        audio_file_name = audio_cleaned_path.split("/")[-1]
-        if not os.path.isfile(audio_file_name):
-            audio_url = base_url + audio_cleaned_path
-            doc = requests.get(audio_url)
+        soup = get_soup(page_url)
 
-            with open(audio_file_name, 'wb') as f:
+        # iterate through all phrases on the page
+        for phrase in soup.find_all("div", class_="col-sm-3"):
+            english = phrase.find("span", class_="Stil36").get_text()
+            hindi = phrase.find("span", class_="Stil46").get_text()
+            transliteration = phrase.find("span", class_="Stil39").get_text()
+
+            # clean up image url
+            img_url = base_url + \
+                remove_prefix(phrase.find("img").get("src"), "../../../")
+
+            # Download jpg instead of using remote image
+            image_file_name = "Hindi" + img_url.split("/")[-1]
+            doc = requests.get(img_url)
+            with open(image_file_name, 'wb') as f:
                 f.write(doc.content)
+            anki_formatted_image = f'<img src="{image_file_name}">'
 
-        anki_formatted_audio = "[sound:" + audio_file_name + "]"
+            # add to list of media files to attach to anki package
+            list_of_media_files.append(image_file_name)
 
-        # try not to clobber the server by mass downloading a bunch of files
-        time.sleep(1)
+            # clean up audio url and download mp3
+            audio_cleaned_path = remove_prefix(phrase.find(
+                "source").get("src"), "../../../")
+            audio_file_name = "Hindi" + audio_cleaned_path.split("/")[-1]
+            if not os.path.isfile(audio_file_name):
+                audio_url = base_url + audio_cleaned_path
+                doc = requests.get(audio_url)
 
-        # create card and add to deck
-        new_note = genanki.Note(
-            anki_card_model, [english, hindi, transliteration, anki_formatted_audio, img_url])
-        new_deck.add_note(new_note)
+                with open(audio_file_name, 'wb') as f:
+                    f.write(doc.content)
 
-        print(new_note)
-        print("\n")
+            anki_formatted_audio = "[sound:" + audio_file_name + "]"
 
-    genanki.Package(new_deck).write_to_file('output.apkg')
+            # add to list of media files to attach to anki package
+            list_of_media_files.append(audio_file_name)
+
+            # try not to clobber the server by mass downloading a bunch of files
+            time.sleep(1)
+
+            # create card and add to deck
+            new_note = genanki.Note(
+                anki_card_model, [english, hindi, transliteration, anki_formatted_audio, anki_formatted_image])
+            new_deck.add_note(new_note)
+            print(new_note)
+            print("\n")
+            break
+
+    new_package = genanki.Package(new_deck)
+    new_package.media_files = list_of_media_files
+    new_package.write_to_file('output.apkg')
+
     return new_deck
 
 
